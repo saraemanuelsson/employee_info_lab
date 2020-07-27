b1f9fd9d702442b83533036a0922eb99dfbc4513
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-27 15:56:07 +0100

    Add director subclass with private property of budget and tests for all properties, inherited and private

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Director;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    private Director director;
+
+    @Before
+    public void before() {
+        director = new Director("CEO Cindy", "007", 150000.00, "Exec", 1000000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("CEO Cindy", director.getName());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("007", director.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(150000.00, director.getSalary(), 0.001);
+    }
+
+    @Test
+    public void hasDeptName() {
+        assertEquals("Exec", director.getDeptName());
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(1000000.00, director.getBudget(), 0.001);
+    }
+
+
+}
