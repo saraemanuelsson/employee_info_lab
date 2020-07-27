62826bced7cafdddb5b4603085ab904180c0b7b4
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-27 14:40:32 +0100

    Add manager subclass of employee and tests to check for name, ni number and salary

---
+++ src/main/java/staff/Manager.java
@@ -0,0 +1,11 @@
+package staff;
+
+public class Manager extends Employee {
+
+    public Manager(String name, String niNumber, double salary) {
+        super(name, niNumber, salary);
+    }
+
+
+
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    private Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("Boss Brian", "123", 28000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Boss Brian", manager.getName());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("123", manager.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(28000.00, manager.getSalary(), 0.001);
+    }
+
+}
